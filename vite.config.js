3183544cd4bc3f4ae66f7984c087f3510161ba0a
Muhammet Talha Akyüz <talhaakyuz1661@gmail.com>
2026-05-03 12:55:24 +0300

    PWA eklendi

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path' // Bu satırı ekle
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'KeepTime',
+        short_name: 'App',
+        description: 'Benim Vue uygulamam',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // @ işaretini src klasörüne yönlendirir
     },
   },
 })
-
-
-/*import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // @ işaretini src klasörüne yönlendirir
-    },
-  },
-})*/
